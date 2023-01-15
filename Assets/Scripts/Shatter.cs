da3ad49473dda515b0873a87dfc4ee9c54f7e6c8
mariuszgajewski13 <79981871+mariuszgajewski13@users.noreply.github.com>
2023-01-15 19:12:42 +0100

    Can't count how many things

---
+++ Assets/Scripts/Shatter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shatter : MonoBehaviour
+{
+    public float radius = 100f;
+    public float power = 10F;
+    public ParticleSystem explosion;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+        explosion.Play();
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            Debug.Log("BOOM");
+            if (rb != null)
+                rb.AddExplosionForce(power, explosionPos, radius, 4);
+        }
+    }
+}
