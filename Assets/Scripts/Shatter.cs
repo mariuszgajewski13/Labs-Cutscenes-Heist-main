3a58bc112692e4a015f1f7bd6a51d207250266f2
mariuszgajewski13 <79981871+mariuszgajewski13@users.noreply.github.com>
2023-01-24 00:12:33 +0100

    Almost at the finish line

--- Assets/Scripts/Shatter.cs
+++ Assets/Scripts/Shatter.cs
@@ -8,7 +8,7 @@ public class Shatter : MonoBehaviour
     public float power = 10F;
     public ParticleSystem explosion;
 
-    private void OnTriggerEnter(Collider other)
+    private void Start()
     {
         Vector3 explosionPos = transform.position;
         Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
@@ -18,7 +18,7 @@ public class Shatter : MonoBehaviour
             Rigidbody rb = hit.GetComponent<Rigidbody>();
             Debug.Log("BOOM");
             if (rb != null)
-                rb.AddExplosionForce(power, explosionPos, radius, 4);
+                rb.AddExplosionForce(power, explosionPos, radius, 3);
         }
     }
 }
